c6fea05942cec5be32cc919bee923658e6b2f8a6
Chandan Jade <2chandanjade@gmail.com>
2017-08-19 13:45:38 +0530

    Ensures Cell comparison works as expected

--- src/com/Cell.java
+++ src/com/Cell.java
@@ -13,6 +13,21 @@ public class Cell {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cell cell = (Cell) o;
+
+        return value == cell.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value;
+    }
+
+    @Override
     public String toString() {
         return "" + value;
     }
